c3ef771e63865177801906f5e9c4e76d8c061c50
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-06-29 18:21:29 +0200

    Add project files

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', (err, data) => {
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        });
+
+    } else {
+        response.setHeader("Content-Type", "image/png");
+        fs.readFile('./error.png', (err, data2) => {
+            if (err) throw err;
+            response.statusCode = 404;
+            response.write(data2);
+            response.end();
+        })
+    }
+});
+
+server.listen(8080);
